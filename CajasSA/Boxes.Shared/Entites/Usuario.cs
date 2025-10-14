9ade0750d1163402604d5367d775c14e2551b79b
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-13 21:50:26 -0500

    si

---
+++ CajasSA/Boxes.Shared/Entites/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boxes.Shared.Entites;
+
+public class Usuario
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El campo {0} debe de ser obligatorio")]
+    [MaxLength(30, ErrorMessage = "el campo{0} no puede tener {1} caracteres")]
+    public string Nombre { get; set; } = null!;
+}
