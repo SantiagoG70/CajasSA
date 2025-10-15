c8c15aff3df21adcc43e7532b16c6bc8c48ddb95
Tomas Arias <tomasariasuribe302@gmail.com>
2025-10-15 18:38:18 -0500

    agregando data annotations

--- CajasSA/Boxes.Shared/Entites/Proveedor.cs
+++ CajasSA/Boxes.Shared/Entites/Proveedor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,21 @@ namespace Boxes.Shared.Entites
     public class Proveedor
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nombre proveedor")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "el nombre dene tener entre 3 y 50 caracteres")]
+        [Required]
         public string Name { get; set; } = null!;
+
+        [Display(Name = "Telefono")]
+        [Phone(ErrorMessage = "El número de teléfono no es válido.")]
+        [StringLength(20)]
+        [Required]
         public string Phone { get; set; } = null!;
+
+        [Display(Name = "Direccion")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "la direccion dene tener entre 10 y 100 caracteres")]
+        [Required]
         public string Address { get; set; } = null!;
         public Producto ProductType { get; set; } = null!;
 
