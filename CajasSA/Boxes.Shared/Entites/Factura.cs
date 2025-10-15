87ff897377ccc35db814c8b9d8dc9afb5a50adbf
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-15 17:32:29 -0500

    Modificacion de clases

--- CajasSA/Boxes.Shared/Entites/Factura.cs
+++ CajasSA/Boxes.Shared/Entites/Factura.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Boxes.Shared.Entites
+namespace Boxes.Shared.Entites;
+
+public class Factura
 {
-    public class Factura
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Display(Name = "Fecha de emision")]
-        public DateTime Fecha { get; set; } 
-        public int ClienteId { get; set; }
-        public Cliente? Cliente { get; set; } = null!;
-    }
+    [Display(Name = "Fecha de emision")]
+    public DateTime Fecha { get; set; }
+
+    public int ClienteId { get; set; }
+    public Cliente? Cliente { get; set; } = null!;
 }
