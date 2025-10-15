cfab11a45d9d85aa55228f16838d30fd93cf7cf4
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-15 18:18:33 -0500

    Carritos y dependencias

--- CajasSA/Boxes.Shared/Entites/Cliente.cs
+++ CajasSA/Boxes.Shared/Entites/Cliente.cs
@@ -9,6 +9,8 @@ public class Cliente
     [Required(ErrorMessage = "La direccion es obligatoria")]
     public string Direccion { get; set; } = null!;
 
+    public int CarritoId { get; set; }
+    public Carrito? Carrito { get; set; }
     public ICollection<Factura>? Facturas { get; set; } = null!;
     public int UsuarioId { get; set; }
     public Usuario? Usuario { get; set; }
--- CajasSA/Boxes.Shared/Entites/Factura.cs
+++ CajasSA/Boxes.Shared/Entites/Factura.cs
@@ -7,8 +7,11 @@ public class Factura
     public int Id { get; set; }
 
     [Display(Name = "Fecha de emision")]
-    public DateTime Fecha { get; set; }
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public DateTime Date { get; set; }
 
+    public int CarritoId { get; set; }
+    public Carrito? Carrito { get; set; }
     public int ClienteId { get; set; }
     public Cliente? Cliente { get; set; } = null!;
 }
