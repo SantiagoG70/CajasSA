44d6448244fd5cd3d05a0bcf9f509f3a2da081d6
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-15 19:52:21 -0500

    Inventario

---
+++ CajasSA/Boxes.Shared/Entites/Inventario.cs
@@ -0,0 +1,9 @@
+﻿namespace Boxes.Shared.Entites;
+
+public class Inventario
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public int Stock { get; set; }
+    public ICollection<Producto>? Productos { get; set; } = new List<Producto>();
+}
