b2713b0d5a3c12c99a287a5e4a790d64d2f0364f
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-17 12:47:27 -0500

    Bdset

--- CajasSA/boxes.Backend/Data/DataContext.cs
+++ CajasSA/boxes.Backend/Data/DataContext.cs
@@ -10,6 +10,19 @@ public class DataContext : DbContext
     }
 
     public DbSet<Rol> Roles { get; set; }
+    public DbSet<Administrador> Administradores { get; set; }
+    public DbSet<Alerta> Alertas { get; set; }
+    public DbSet<Carrito> Carritos { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<DetalleFactura> DetalleFacturas { get; set; }
+    public DbSet<Empleado> Empleados { get; set; }
+    public DbSet<Factura> Facturas { get; set; }
+    public DbSet<Inventario> Inventarios { get; set; }
+    public DbSet<ItemCarrito> ItemCarritos { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Proveedor> Proveedores { get; set; }
+    public DbSet<Reportes> Reportes { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
