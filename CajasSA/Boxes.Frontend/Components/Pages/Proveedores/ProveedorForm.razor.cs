074c8a1ebbd43bdbb9bfac4f6795f32de1805e9e
Tomas Arias <tomasariasuribe302@gmail.com>
2025-10-22 18:48:21 -0500

    agregando formulario de proveedor

---
+++ CajasSA/Boxes.Frontend/Components/Pages/Proveedores/ProveedorForm.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Boxes.Shared.Entites;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Boxes.Frontend.Components.Pages.Proveedores
+{
+    public partial class ProveedorForm
+    {
+        private EditContext editContext = null!;
+
+        [EditorRequired, Parameter] public Employee Employee { get; set; } = null!;
+        [EditorRequired, Parameter] public EventCallback OnValidSubmit { get; set; }
+        [EditorRequired, Parameter] public EventCallback ReturnAction { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            if (editContext is null || !ReferenceEquals(editContext.Model, Employee))
+                editContext = new EditContext(Employee);
+        }
+
+        protected override void OnInitialized()
+        {
+            editContext = new(Employee);
+        }
+    }
+}
